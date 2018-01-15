95ffde6954fd0e121016b62fa0fa1a1e6df65d90
ewqdsacxz <Patric_emmel@outlook.com>
2018-01-15 20:27:54 +0700

    [ADD] approve empty page

---
+++ pages/approve.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Layout from '../components/layout/layout'
+
+const Approve  = () =>(
+    <div>
+        <Layout subheadertext={'approve'}>
+        </Layout>
+    </div>
+)
+export default Approve
