7f9797a2313398a40055c5e1e834b5a593318e02
ewqdsacxz <Patric_emmel@outlook.com>
2018-01-29 17:25:23 +0700

    [ADD] Tag and Component inside tag

---
+++ pages/prove.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import Layout from '../components/layout/layout'
+import Tab1 from '../components/prove/tab1'
+import { Tab } from 'semantic-ui-react'
+
+class Prove extends React.Component {
+  render () {
+    const panes = [
+      { menuItem: 'ข้อมูลน้อง', render: () => <Tab.Pane attached={false}><Tab1 /></Tab.Pane> },
+      { menuItem: 'ปพ.1', render: () => <Tab.Pane attached={false}>Tab 2 Content</Tab.Pane> },
+      { menuItem: 'ใบอนุญาติ', render: () => <Tab.Pane attached={false}>Tab 3 Content</Tab.Pane> }
+    ]
+
+    return (
+      <div>
+        <Layout>
+          <Tab menu={{ pointing: true }} panes={panes} />
+        </Layout>
+      </div>
+    )
+  }
+}
+
+export default Prove
