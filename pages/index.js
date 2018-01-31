0b91e58e8f93c52b7e142e0b313e78bcc5769c91
Phachara Kamthong <pcr.mixkungz@hotmail.com>
2018-01-31 22:30:21 +0700

    [UPDATE] Change to use new layout with semantic

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,13 @@
 import React from 'react'
 import Layout from '../components/layout/layout'
-import { Button } from 'semantic-ui-react'
+import { Button, Grid } from 'semantic-ui-react'
 
 export default props => (
   <Layout subheadertext='Dashboard'>
-    <Button>Click Here</Button>
+    <Grid.Row>
+      <Grid.Column>
+        <Button>Click Here</Button>
+      </Grid.Column>
+    </Grid.Row>
   </Layout>
 )
