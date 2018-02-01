6ce64abfeff49b06c69ee02ffca753433bf56227
Phachara Kamthong <pcr.mixkungz@hotmail.com>
2018-02-01 20:09:29 +0700

    [UPDATE] Change UI

--- pages/login.js
+++ pages/login.js
@@ -2,14 +2,18 @@ import React from 'react'
 import styled from 'styled-components'
 
 const Wrapper = styled.div`
+  min-height:70vh;
+  justify-content:center;
+  align-items:center;
+  display:flex;
+  flex-direction: column;
+`
+const BackgroundWrapper = styled.div`
   background-image:url('/static/img/loginbg.png');
   background-size: cover;
   width:100%;
   height:100vh;
   max-height:100vh;
-  justify-content:center;
-  align-items:center;
-  display:flex;
 `
 const Box = styled.div`
   box-shadow : 0px 1px 15px 1px rgba(81, 77, 92, 0.08);
@@ -27,18 +31,20 @@ const FacebookLogin = styled.div`
 `
 const Topic = styled.h1`
   color:white;
-  border-right:2px solid white;
-  padding-right:10px;
-  margin-right:15px;
+  font-size:5em;
+  text-shadow: -1px 0px 25px rgba(81, 77, 92, 0.6);
+  text-align:center;
 `
 class Login extends React.Component {
   render () {
     return (
       <div>
-        <Wrapper>
-          <Topic>WIPCAMP<br/>MANAGEMENT</Topic>
-          <Box><FacebookLogin>Login with facebook</FacebookLogin></Box>
-        </Wrapper>
+        <BackgroundWrapper>
+          <Wrapper>
+            <Topic>WIPCAMP<br />MANAGEMENT</Topic>
+            <Box><FacebookLogin>Login with facebook</FacebookLogin></Box>
+          </Wrapper>
+        </BackgroundWrapper>
       </div>
     )
   }
