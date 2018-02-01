ec5694b2bbcd0a5ed7336c6fba8071d050752724
Phachara Kamthong <pcr.mixkungz@hotmail.com>
2018-02-01 19:42:30 +0700

    [UPDATE] Change injectGlobal to styled

--- components/layout/menu.js
+++ components/layout/menu.js
@@ -1,37 +1,41 @@
 import React from 'react'
-import { injectGlobal } from 'styled-components'
+import styled from 'styled-components'
 import { Menu } from 'semantic-ui-react'
 
-injectGlobal`
-  .menu-bg{
-    background:transparent !important;
-    border:0 !important;
-    box-shadow:none !important;
-  }
-  .menu-bg .item{
-    padding:.92857143em 1.64285714em;
-    font-size:1.1em;
-  }
-  .menu-bg .item.active{
+const StyledMenu = styled(Menu)`
+  background:transparent !important;
+  border:0 !important;
+  box-shadow:none !important;
+`
+const StyledMenuItem = styled(Menu.Item)`
+  padding:.92857143em 1.64285714em !important;
+  font-size:1.1em !important;
+  color:#fff !important;
+  &:active{
     background:rgb(58, 165, 212) !important;
   }
-  .menu-bg .item:before{
+  &:before{
     width:0 !important;
   }
-`
 
-const MyMenu = () => (
-  <Menu className='menu-bg'>
-    <Menu.Item active>
-      Dashboard
-    </Menu.Item>
-    <Menu.Item>
-      Document
-    </Menu.Item>
-    <Menu.Item>
-      Itim Management
-    </Menu.Item>
-  </Menu>
-  
-)
+  ${props => props.active === 'dashboard' && `
+    background:rgb(58, 165, 212) !important;
+  `}
+`
+const MenuNames = ['Dashboard', 'Document', 'Itim Management']
+
+const ListMenu = MenuNames.map((MenuName) => <StyledMenuItem active={this.state.page} >{MenuName}</StyledMenuItem>)
+
+class MyMenu extends React.Component {
+  render () {
+    return (
+      <StyledMenu>
+        {
+          ListMenu
+        }
+      </StyledMenu>
+    )
+  }
+}
+
 export default MyMenu
