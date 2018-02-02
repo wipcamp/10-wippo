be084411f0bce9c6c6b0bf044a3cacb536d2703a
ewqdsacxz <Patric_emmel@outlook.com>
2018-02-02 15:42:17 +0700

    [UPDATE]adjust document

--- components/prove/tab2.js
+++ components/prove/tab2.js
@@ -1,8 +1,12 @@
 import React from 'react'
 import ItimCard from '../general/itimcard'
-import {Grid, Image, Button} from 'semantic-ui-react'
+import {Grid, Image, Button, Segment} from 'semantic-ui-react'
+import styled from 'styled-components'
 const ItimImage = '/static/img/itim-profileImg.jpg'
 const transcript = '/static/img/transcript.jpg'
+const EnhancedImage = styled(Image)`
+margin:2%;
+`
 
 class Tab2 extends React.Component {
   render () {
@@ -13,18 +17,17 @@ class Tab2 extends React.Component {
             <Grid.Column width={4}>
               <ItimCard src={ItimImage} name={'Phachara kamdor'} school={'kmutt'} join={'30 JAN 2018'} />
             </Grid.Column>
-            <Grid.Column width={12}>
-              ปพ.
-              <Image src={transcript} fluid />
+            <Grid.Column width={12} textAlign={'center'}>
+              <EnhancedImage src={transcript} size={'large'} centered />
+              <Button.Group>
+                <Button>Reject</Button>
+                <Button.Or />
+                <Button color='yellow'>Pending</Button>
+                <Button.Or />
+                <Button >Approved</Button>
+              </Button.Group>
             </Grid.Column>
           </Grid.Row>
-          <Button.Group>
-            <Button>Reject</Button>
-            <Button.Or />
-            <Button color='yellow'>Pending</Button>
-            <Button.Or />
-            <Button >Approved</Button>
-          </Button.Group>
         </Grid>
       </div>
     )
