627ec847e8705ce2c920cbfae6aecca20cfdb6ca
Phachara Kamthong <pcr.mixkungz@hotmail.com>
2018-02-18 23:57:59 +0700

    [UPDATE] Send Url Props to component

--- pages/wipper.js
+++ pages/wipper.js
@@ -3,15 +3,13 @@ import Layout from '../components/layout/layout'
 import { Grid } from 'semantic-ui-react'
 import WipperMain from '../components/wipper/Main'
 class Wipper extends React.Component {
-  state = {
 
-  }
   render () {
     return (
       <Layout subheadertext='WIPPER Management'>
         <Grid.Row>
           <Grid.Column>
-            <WipperMain></WipperMain>
+            <WipperMain {...this.props} />
           </Grid.Column>
         </Grid.Row>
       </Layout>
