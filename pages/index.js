824f6c6a55fca8a4a090b10731bc7562142b2f8c
Keerati <mixxdarkz2@gmail.com>
2018-02-19 20:51:45 +0700

    [REMOVE] Remvoe InjectGlobal

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,7 @@
 import React from 'react'
-import injectGlobal from '../components/layout/injectGlobal'
 import IndexPage from '../components/index/Main'
 
 class Login extends React.Component {
-  componentWillMount () {
-    injectGlobal
-  }
   render () {
     return (
       <IndexPage />
