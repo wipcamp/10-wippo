36889416f4831b521ea82530ae7e46c1266399f3
Keerati <mixxdarkz2@gmail.com>
2018-02-19 20:51:20 +0700

    [ADD] Add Font Kanit

--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,7 @@
 import Document, { Head, Main, NextScript } from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
 import htmlescape from 'htmlescape'
+import injectGlobal from '../components/layout/injectGlobal'
 
 const { API_URL, URL } = process.env
 const env = { API_URL, URL }
@@ -14,6 +15,10 @@ export default class MyDocument extends Document {
     return { ...page, styleTags }
   }
 
+  componentWillMount() {
+    injectGlobal
+  }
+
   render () {
     return (
       <html>
@@ -22,8 +27,7 @@ export default class MyDocument extends Document {
           <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
           <meta httpEquiv="X-UA-Compatible" content="ie=edge"/>
           <title>WIPPO | WIP Camp #10</title>
-          <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,600" rel="stylesheet"/>
-          <link href="https://fonts.googleapis.com/css?family=Roboto:400,500" rel="stylesheet"/>
+          <link href="https://fonts.googleapis.com/css?family=Kanit" rel="stylesheet" />
           <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossOrigin="anonymous" />
           <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.12/semantic.min.css" />
           <link rel="stylesheet" href="/static/css/style.css"/>
