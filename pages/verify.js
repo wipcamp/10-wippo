5622fd23fe58e5abe1ff3e0b10e97c75641fc6c0
Keerati <mixxdarkz2@gmail.com>
2018-02-19 13:59:49 +0700

    [UPDATE] Update Verify Page for used cookie

--- pages/verify.js
+++ pages/verify.js
@@ -3,12 +3,20 @@ import Layout from '../components/layout/layout'
 import Tab1 from '../components/verify/tab1'
 import Tab2 from '../components/verify/tab2'
 import Tab3 from '../components/verify/tab3'
-import axios from '../utils/axios'
-import Querystring from 'query-string'
+import axios from '../components/util/axios'
+import getCookie from '../components/util/cookie'
 import { Tab, Button } from 'semantic-ui-react'
 
+const filterDocument = (doc, typeId) => {
+  let documents = []
+  doc.filter(data => {
+    documents[data.type_id - 1] = data.path
+  })
+  return documents
+}
+
 class Verify extends React.Component {
-  constructor (props) {
+  constructor(props) {
     super()
     this.state = {
       profile: {
@@ -20,16 +28,20 @@ class Verify extends React.Component {
           edu_name: ''
         }
       },
-      parentPermission: '',
-      transcript: ''
+      documents: []
     }
     this.handlePutData = this.handlePutData.bind(this)
   }
 
-  async componentWillMount () {
-    const {data} = await axios.get('/registrants/' + this.props.url.query.user_id)
-    await this.setState({profile: data[0]})
+  async componentDidMount () {
+    let { token } = await getCookie({ req: false })
+    let { data } = await axios.get(`/registrants/${this.props.url.query.user_id}`, {
+      Authorization: `Bearer ${token}`
+    })
+    await this.setState({ profile: data[0] })
     console.log(this.state.profile)
+    await this.setState({ documents: filterDocument(this.state.profile.documents) })
+    console.log(filterDocument(this.state.profile.documents))
   }
 
   async handlePutData (...e) {
@@ -42,18 +54,39 @@ class Verify extends React.Component {
 
   render () {
     const panes = [
-      { menuItem: 'ข้อมูลน้อง', render: () => <Tab.Pane attached={false}><Tab1 info={this.state.profile} /></Tab.Pane> },
-      { menuItem: 'ปพ.1', render: () => <Tab.Pane attached={false}><Tab2 info={this.state.profile} status={this.state.transcript} button={<ButtonTranscript />} /></Tab.Pane> },
-      { menuItem: 'ใบอนุญาติ', render: () => <Tab.Pane attached={false}><Tab3 info={this.state.profile} status={this.state.parentPermission} button={<ButtonParentPermission />} /></Tab.Pane> }
+      { menuItem: 'ข้อมูลน้อง',
+        render: () => <Tab.Pane attached={false}>
+          <Tab1
+            image={this.state.documents[0]}
+            info={this.state.profile} />
+        </Tab.Pane> },
+      { menuItem: 'ปพ.1',
+        render: props => <Tab.Pane attached={false}>
+          <Tab2
+            image={this.state.documents[0]}
+            path={this.state.documents[2]}
+            info={this.state.profile}
+            status={this.state.transcript}
+            button={<ButtonTranscript />} />
+        </Tab.Pane> },
+      { menuItem: 'ใบอนุญาติ',
+        render: props => <Tab.Pane attached={false}>
+          <Tab3
+            image={this.state.documents[0]}
+            path={this.state.documents[1]}
+            info={this.state.profile}
+            status={this.state.parentPermission}
+            button={<ButtonParentPermission />} />
+        </Tab.Pane> }
     ]
 
     const ButtonParentPermission = () => (
       <Button.Group>
-        {this.state.parentPermission === 'reject' ? <Button color='red' >Reject </Button> : <Button onClick={() => this.setState({parentPermission: 'reject'})} >Reject </Button>}
+        {this.state.parentPermission === 'reject' ? <Button color='red' >Reject </Button> : <Button onClick={() => this.setState({ parentPermission: 'reject' })} >Reject </Button>}
         <Button.Or />
-        {this.state.parentPermission === '' ? <Button color='yellow' >Pending</Button> : <Button onClick={() => this.setState({parentPermission: ''})}>Pending</Button>}
+        {this.state.parentPermission === '' ? <Button color='yellow' >Pending</Button> : <Button onClick={() => this.setState({ parentPermission: '' })}>Pending</Button>}
         <Button.Or />
-        {this.state.parentPermission === 'approve' ? <Button color='green' >Approved </Button> : <Button onClick={() => this.setState({parentPermission: 'approve'})}>Approved </Button>}
+        {this.state.parentPermission === 'approve' ? <Button color='green' >Approved </Button> : <Button onClick={() => this.setState({ parentPermission: 'approve' })}>Approved </Button>}
       </Button.Group>
     )
 
@@ -61,16 +94,16 @@ class Verify extends React.Component {
       <Button.Group>
         {this.state.transcript === 'reject' ? <Button color='red' >Reject </Button> : <Button onClick={() => this.handlePutData(12, 0)} >Reject </Button>}
         <Button.Or />
-        {this.state.transcript === '' ? <Button color='yellow' >Pending</Button> : <Button onClick={() => this.setState({transcript: ''})}>Pending</Button>}
+        {this.state.transcript === '' ? <Button color='yellow' >Pending</Button> : <Button onClick={() => this.setState({ transcript: '' })}>Pending</Button>}
         <Button.Or />
-        {this.state.transcript === 'approve' ? <Button color='green' >Approved </Button> : <Button onClick={() => this.setState({transcript: 'approve'})}>Approved </Button>}
+        {this.state.transcript === 'approve' ? <Button color='green' >Approved </Button> : <Button onClick={() => this.setState({ transcript: 'approve' })}>Approved </Button>}
       </Button.Group>
     )
 
     return (
       <div>
         <Layout>
-          <Tab menu={{ pointing: true }} panes={panes} />
+          <Tab {...this.props} menu={{ pointing: true }} panes={panes} />
         </Layout>
       </div>
     )
