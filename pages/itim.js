86ca9dbd1fc1186a5b1a15e278b658ba599d311b
Phachara Kamthong <pcr.mixkungz@hotmail.com>
2018-02-20 19:22:17 +0700

    [UPDATE] Use code from Verify

--- pages/itim.js
+++ pages/itim.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Grid } from 'semantic-ui-react'
 import Layout from '../components/layout/layout'
 import DatatableCard from '../components/itim/DatatableCard'
-import ItimProfile from './verify'
+import ItimProfile from '../components/itim/itimprofile'
 class Itim extends React.Component {
   render () {
     return (
