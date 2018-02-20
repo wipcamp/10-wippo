729353cbafe54d3d6dd2c27561db8662e439762b
Phachara Kamthong <pcr.mixkungz@hotmail.com>
2018-02-20 18:56:11 +0700

    [UPDATE] Change ItimProfile to use verify component

--- pages/itim.js
+++ pages/itim.js
@@ -2,16 +2,16 @@ import React from 'react'
 import { Grid } from 'semantic-ui-react'
 import Layout from '../components/layout/layout'
 import DatatableCard from '../components/itim/DatatableCard'
-import ItimProfile from '../components/itim/itimprofile'
+import ItimProfile from './verify'
 class Itim extends React.Component {
   render () {
     return (
       <Layout subheadertext='Itim Management'>
         <Grid.Row>
           {
-            this.props.url.query.id ? (
+            this.props.url.query.user_id ? (
               <Grid.Column>
-                <ItimProfile id={this.props.url.query.id} />
+                <ItimProfile user_id={this.props.url.query.user_id} {...this.props} />
               </Grid.Column>
             ) : (
               <Grid.Column>
