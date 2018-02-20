86fd0dd822e0a4675e1e781d65bcdc141be2db48
Keerati <mixxdarkz2@gmail.com>
2018-02-20 22:48:35 +0700

    [UPDATE] Update Name in Card

--- components/general/itimcard.js
+++ components/general/itimcard.js
@@ -1,19 +1,19 @@
 import React from 'react'
 import {Card, Image} from 'semantic-ui-react'
 
-const ItimCard = ({src, name, join, school}) => (
+const ItimCard = ({fullName, src, name, join, school}) => (
   <div>
     <Card>
       <Image centered fluid src={src} />
       <Card.Content>
         <Card.Header>
           <div className='text-center'>
-            {name}
+            <h1>{fullName}</h1>
           </div>
         </Card.Header>
         <Card.Meta>
-          <div className='date text-center'>
-            {`Joined Date: ${new Date(join).toLocaleDateString()}`}
+          <div className='text-center'>
+            <h1>{name}</h1>
           </div>
         </Card.Meta>
         <Card.Description>
