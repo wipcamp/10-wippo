2d31ed50b2ef475bee40835d195df0380162c152
ewqdsacxz <Patric_emmel@outlook.com>
2018-03-10 19:57:26 +0700

    [ADD]Itim answerPage

---
+++ pages/itimanswer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Layout from '../components/layout/layout'
+
+
+export default class Itimanswer extends React.Component {
+  render () {
+    return (
+      <Layout>
+        ItimAnswer
+      </Layout>
+    )
+  }
+}
