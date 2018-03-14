34b453835e99b0d1e25717aa5bc2a9cb8e3ce4b9
ewqdsacxz <Patric_emmel@outlook.com>
2018-03-14 21:46:42 +0700

    [ADD] Semantic Tab pane

--- pages/itimanswer.js
+++ pages/itimanswer.js
@@ -1,12 +1,26 @@
 import React from 'react'
 import Layout from '../components/layout/layout'
-
+import {Tab} from 'semantic-ui-react'
 
 export default class Itimanswer extends React.Component {
   render () {
+    const Breadcrumb = () => <ol className='breadcrumb'>
+      <li className='breadcrumb-item'><a href='/checkanswer'>checkanswer</a></li>
+      <li className='breadcrumb-item active' aria-current='page'>ItimAnswer</li>
+    </ol>
+
+    const panes = [
+      {menuItem: 'ข้อมูลน้อง',
+        render: () => <Tab.Pane attached={false}>
+          <Tab1 />> 
+        </Tab.Pane>
+      }
+    ]
+
     return (
-      <Layout>
+      <Layout subheadertext={<Breadcrumb />}>
         ItimAnswer
+        <Tab menu={{ pointing: true }} panes={panes} />
       </Layout>
     )
   }
