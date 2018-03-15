b04b57f3237c1426e7f0d79c0e8197a0b283ce76
ewqdsacxz <Patric_emmel@outlook.com>
2018-03-15 19:54:27 +0700

    [ADD] fetch itim data

--- pages/itimanswer.js
+++ pages/itimanswer.js
@@ -1,8 +1,21 @@
 import React from 'react'
 import Layout from '../components/layout/layout'
 import {Tab} from 'semantic-ui-react'
+import Tab1 from '../components/verify/tab1'
+import axios from '../components/util/axios'
+import getCookie from '../components/util/cookie'
+
 
 export default class Itimanswer extends React.Component {
+  async componentWillMount () {
+    let { token } = await getCookie({ req: false })
+    let { data } = await axios.get(`/registrants/${this.props.url.query.user_id}`, {
+      Authorization: `Bearer ${token}`
+    })
+    console.log('data', data[0])
+    console.log('token', token)
+  }
+
   render () {
     const Breadcrumb = () => <ol className='breadcrumb'>
       <li className='breadcrumb-item'><a href='/checkanswer'>checkanswer</a></li>
@@ -12,7 +25,7 @@ export default class Itimanswer extends React.Component {
     const panes = [
       {menuItem: 'ข้อมูลน้อง',
         render: () => <Tab.Pane attached={false}>
-          <Tab1 />> 
+          {/* <Tab1 /> */}
         </Tab.Pane>
       }
     ]
