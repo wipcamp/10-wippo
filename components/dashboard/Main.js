98f00eaf5fd1659c7dead8e27564250d567e81fc
Phachara Kamthong <pcr.mixkungz@hotmail.com>
2018-03-17 04:52:30 +0700

    [UPDATE] Add Mockup Chart

--- components/dashboard/Main.js
+++ components/dashboard/Main.js
@@ -5,6 +5,7 @@ import Portlet from '../../components/util/portlet'
 import axios from '../../components/util/axios'
 import getCookie from '../../components/util/cookie'
 import Moment from 'moment'
+import {Doughnut ,Line} from 'react-chartjs-2'
 
 const Header = styled.h3`
   font-weight: 500;
@@ -53,8 +54,40 @@ class Main extends React.Component {
     })
   }
   render () {
+    const data = {
+      labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+      datasets: [{
+        label: '# of Votes',
+        data: [12, 19, 3, 5, 2, 3],
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)'
+        ],
+        borderColor: [
+          'rgba(255,99,132,1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)'
+        ],
+        borderWidth: 1
+      }]
+    }
     return (
       <Layout subheadertext='Dashboard'>
+        <div className='row'>
+          <div className='col'>
+            <Doughnut ref='chart' data={data} />
+          </div>
+          <div className='col'>
+            <Line data={data} />
+          </div>
+        </div>
         <div className='row'>
           <div className='col-12 col-md-4 mb-4'>
             <Portlet title='น้องใหม่ประจำวัน' herotext={`${this.state.registerAmount} คน`} image='/static/img/team.svg' />
