9ccde842d8f5b164ec6edd36c1cf737a09e200a7
Phachara Kamthong <pcr.mixkungz@hotmail.com>
2018-03-19 09:58:31 +0700

    [ADD] Create CheckAnswerChart.js

---
+++ components/dashboard/CheckAnswerChart.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import {HorizontalBar} from 'react-chartjs-2'
+import axios from '../../components/util/axios'
+import getCookie from '../../components/util/cookie'
+
+class CheckAnswerChart extends React.Component {
+  state = {
+    data: {
+      labels: ['ข้อที่ 1', 'ข้อที่ 2', 'ข้อที่ 3', 'ข้อที่ 4', 'ข้อที่ 5', 'ข้อที่ 6'],
+      datasets: [{
+        label: '% of Success',
+        data: [12, 59, 3, 5, 2, 100],
+        backgroundColor: [
+          '#45A7D0',
+          '#45A7D0',
+          '#45A7D0',
+          '#45A7D0',
+          '#45A7D0',
+          '#45A7D0'
+        ]
+      }]
+    },
+    registerSuccess: 0
+  }
+  componentDidMount = async () => {
+    let {token} = await getCookie({req: false})
+    let headers = {
+      Authorization: `Bearer ${token}`
+    }
+    let registerSuccess = await axios.get('/dashboard/register/success', headers)
+    await this.setState({
+      registerSuccess: registerSuccess.data.length
+    })
+  }
+
+  render () {
+    console.log(this.state.registerSuccess)
+    return (
+      <div>
+        <HorizontalBar data={this.state.data} />
+      </div>
+    )
+  }
+}
+
+export default CheckAnswerChart
