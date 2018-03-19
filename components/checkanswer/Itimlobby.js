de4c5866eb5a4c490ea287cc6be455ed1505fad3
ewqdsacxz <Patric_emmel@outlook.com>
2018-03-19 17:12:49 +0700

    [ADD] fetch ItimQuestion by role

--- components/checkanswer/Itimlobby.js
+++ components/checkanswer/Itimlobby.js
@@ -1,7 +1,25 @@
 import React from 'react'
 import ApproveTable from './ApproveTable'
+import axios from '../util/axios'
+import getCookie from '../util/cookie'
 
 export default class Itimlobby extends React.Component {
+  async componentDidMount () {
+    const question = []
+    let { token } = await getCookie({ req: false })
+    const teams = JSON.parse(
+      window.localStorage.getItem('team')
+    )
+    teams.map(team => {
+      let data = axios.get(`/answers/${team.role}`, {
+        Authorization: `Bearer ${token}`
+      })
+      data.then(val => {
+        question.push(val.data.data)
+      })
+    })
+    console.log('Question', question)
+  }
   render () {
     return (
       <div style={{width: '100%'}} >
--- components/util/axios.js
+++ components/util/axios.js
@@ -3,7 +3,7 @@ import env from './env'
 
 const createInstance = (headers) => {
   return axios.create({
-    baseURL: env.API_URL,
+    baseURL: 'http://localhost:8000/api/v1',
     headers
   })
 }
