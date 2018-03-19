9d678c8b58a6bd0e7aa87d4ef0b0e9c9a5b4a249
Phachara Kamthong <pcr.mixkungz@hotmail.com>
2018-03-19 13:26:58 +0700

    [UPDATE] Use Phase1 & Phase2 Component

--- components/dashboard/Main.js
+++ components/dashboard/Main.js
@@ -1,19 +1,19 @@
 import React from 'react'
 import Layout from '../../components/layout/layout'
-import CheckAnswerChart from '../../components/dashboard/CheckAnswerChart'
 import Phase1 from '../../components/dashboard/Phase1'
+import Phase2 from '../../components/dashboard/Phase2'
 class Main extends React.Component {
   render () {
     return (
       <Layout subheadertext='Dashboard'>
         <div className='row'>
           <div className='col'>
-            <CheckAnswerChart />
+            <Phase1 />
           </div>
         </div>
         <div className='row'>
           <div className='col'>
-            <Phase1 />
+            <Phase2 />
           </div>
         </div>
       </Layout>
