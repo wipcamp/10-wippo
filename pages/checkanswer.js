bb6673e92bd8b8717d0ce3f465372e9f924f99de
ewqdsacxz <Patric_emmel@outlook.com>
2018-03-23 19:18:51 +0700

    [UPDATE] UI

--- pages/checkanswer.js
+++ pages/checkanswer.js
@@ -3,10 +3,9 @@ import Layout from '../components/layout/layout'
 import SelectQuestion from '../components/checkanswer/SelectQuestion'
 import Itimlobby from '../components/checkanswer/Itimlobby'
 
-
 const Breadcrumb = () => (
   <ol className='breadcrumb'>
-    <li className='breadcrumb-item active'>Select Question</li>
+    <li className='breadcrumb-item active'>CheckAnswer</li>
   </ol>
 )
 const CheckAnswer = () => (
