b4b509037ff01c654cd756c46e1a4bb698f10fac
ewqdsacxz <Patric_emmel@outlook.com>
2018-03-27 07:24:32 +0700

    [UPDATE] itimcard minimixze display

---
+++ components/ItimAnswers/itimcard.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import styled from 'styled-components'
+import {Card} from 'semantic-ui-react'
+
+const Text = styled.h1`
+  font-family: 'Kanit', sans-serif;
+`
+
+const UserHeader = Text.extend`
+  margin: 0;
+`
+
+const ItimCard = ({id, fullName, name, facebook}) => (
+  <div>
+    <Card>
+      <Card.Content>
+        <Card.Header>
+          <div className='text-center'>
+            <UserHeader>{`wip ID:${id}`}</UserHeader>
+            <UserHeader>{`น้อง ${name}`}</UserHeader>
+          </div>
+        </Card.Header>
+      </Card.Content>
+    </Card>
+  </div>
+)
+
+export default ItimCard
