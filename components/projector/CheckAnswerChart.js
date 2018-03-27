d58d5c69c8ead9e8f8ef2f424d27d719977adfe4
Phachara Kamthong <pcr.mixkungz@hotmail.com>
2018-03-27 17:10:10 +0700

    [UPDATE] Fetch data to graph

--- components/projector/CheckAnswerChart.js
+++ components/projector/CheckAnswerChart.js
@@ -9,7 +9,7 @@ class CheckAnswerChart extends React.Component {
       labels: ['ข้อที่ 1', 'ข้อที่ 2', 'ข้อที่ 3', 'ข้อที่ 4', 'ข้อที่ 5', 'ข้อที่ 6'],
       datasets: [{
         label: '% of Success',
-        data: [12, 59, 3, 5, 2, 100],
+        data: [0, 0, 0, 0, 0, 0],
         backgroundColor: [
           '#45A7D0',
           '#45A7D0',
@@ -20,7 +20,9 @@ class CheckAnswerChart extends React.Component {
         ]
       }]
     },
-    registerSuccess: 0
+    registerSuccess: 0,
+    question: [0, 0, 0, 0, 0, 0],
+    questionPercent: [0, 0, 0, 0, 0, 0]
   }
   componentDidMount = async () => {
     let {token} = await getCookie({req: false})
@@ -28,17 +30,31 @@ class CheckAnswerChart extends React.Component {
       Authorization: `Bearer ${token}`
     }
     let registerSuccess = await axios.get('/dashboard/register/success', headers)
+    let evals = await axios.get('/evals', headers)
+    let question = [0, 0, 0, 0, 0, 0]
+    for (let i = 0; i < evals.data.data.length; i++) {
+      question[evals.data.data[i].eval_answer.question_id - 1]++
+    }
     await this.setState({
-      registerSuccess: registerSuccess.data.length
+      ...this.state,
+      registerSuccess: registerSuccess.data.length,
+      question: question
+    })
+    let questionPercent = [0, 0, 0, 0, 0, 0]
+    for (let i = 0; i < 6; i++) {
+      questionPercent[i] = (this.state.question[i] / this.state.registerSuccess) * 100
+    }
+    let datasets = this.state.data.datasets
+    datasets[0].data = questionPercent
+    await this.setState({
+      ...this.state,
+      data: {
+        datasets
+      }
     })
   }
 
-  percentCalculate = () => {
-    
-  }
-
   render () {
-    console.log(this.state.registerSuccess)
     return (
       <div>
         <HorizontalBar data={this.state.data} />
