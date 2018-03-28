8e07985ad786998bc91fa5bf7980089c957a2f19
Keerati <mixxdarkz2@gmail.com>
2018-03-28 20:36:07 +0700

    [UPDATE] Update Insert and Update Score

--- components/ItimAnswers/Answer.js
+++ components/ItimAnswers/Answer.js
@@ -31,122 +31,116 @@ export default class ItimAnswer extends React.Component {
   constructor () {
     super()
     this.state = {
-      preEval: [],
+      token: '',
+      preEval: {},
       answer: {},
-      itim: {},
+      query: {},
       evals: [],
-      eval1: [],
-      eval2: [],
-      eval3: [],
-      criteria: [],
-      question: { data: null },
-      checker: {}
+      question: { data: null, eval_criteria: [] },
+      eval: [0, 0, 0],
+      criteria: []
     }
-    this.eval1Handeler = this.eval1Handeler.bind(this)
-    this.eval2Handeler = this.eval2Handeler.bind(this)
-    this.eval3Handeler = this.eval3Handeler.bind(this)
-    this.submitHandeler = this.submitHandeler.bind(this)
+  }
+  fetchQuestion = async () => {
+    let {data} = await axios
+      .get(`/questions/${this.state.query.answer}`, {
+        Authorization: `Bearer ${this.state.token}`
+      })
+    this.setState({ question: data[0] })
+  }
+  fetchAnswer = async () => {
+    let {data} = await axios
+      .get(`/answers/answer/${this.state.query.answer}`, {
+        Authorization: `Bearer ${this.state.token}`
+      })
+    this.setState({answer: data[0]})
+  }
+  fetchEvals = async () => {
+    let {data} = await axios.get(`/evals/${this.state.answer.id}`, {
+      Authorization: `Bearer ${this.state.token}`
+    })
+    console.log(data)
+    this.setState({evals: data})
+  }
+  async componentWillMount () {
+    this.setState({
+      query: this.props
+    })
+
+    let preEval = {}
+    preEval.answer_id = +this.props.answer
+    preEval.checker_id = +this.props.id
+    preEval.id = ''
+    preEval.comment = ''
+    preEval.score = 0
+    preEval.criteria_id = ''
+
+    this.setState({preEval})
   }
   async componentDidMount () {
-    let user = window.localStorage.getItem('user')
-    this.setState({checker: JSON.parse(user)})
     let { token } = await getCookie({ req: false })
-    await axios
-      .get(`/answers/answer/${this.props.questionId}`, {
-        Authorization: `Bearer ${token}`
-      })
-      .then(answer => this.setState({ answer: answer.data[0] }))
-    await axios
-      .get(`/profiles/${this.state.answer.user_id}`, {
-        Authorization: `Bearer ${token}`
-      })
-      .then(itim => this.setState({ itim: itim.data }))
-    await axios
-      .get(`/questions/${this.state.answer.question_id}`, {
-        Authorization: `Bearer ${token}`
-      })
-      .then(question => this.setState({ question: question.data[0] }))
-    await axios.get(`evals/criteria/${this.state.answer.question_id}`, {
-      Authorization: `Bearer ${token}`
-    }).then(criteria => this.setState({criteria: criteria.data}))
-    await axios.get(`/evals/${this.state.answer.id}`, {
-      Authorization: `Bearer ${token}`
-    }).then(e => this.setState({evals: e.data}))
+    await this.setState({token})
 
-    let preEval = []
-    preEval['id'] = null
-    preEval['answer_id'] = this.state.answer.question_id
-    preEval['criteria_id'] = this.state.criteria.id
-    preEval['checker_id'] = this.state.checker.id
-    preEval['score'] = 0
-    this.setState({preEval})
+    await this.fetchQuestion()
+    await this.fetchAnswer()
+    await this.fetchEvals()
 
-    console.log('preEval ', preEval)
-    console.log('state eval ', this.state.preEval)
     if (this.state.evals.length > 0) {
-      this.setState({eval1: this.state.evals[0]})
-      this.setState({eval2: this.state.evals[1]})
-      this.setState({eval3: this.state.evals[2]})
+      this.handleEval(0, this.state.evals[0].score)
+      if (this.state.evals[1]) {
+        this.handleEval(1, this.state.evals[1].score)
+      }
+      if (this.state.evals[2]) {
+        this.handleEval(2, this.state.evals[2].score)
+      }
     }
   }
 
-  eval1Handeler (e) {
-    let temp = this.state.eval1
-    temp['score'] = e.target.value
-    this.setState({eval1: temp})
+  handleChange = (field, value) => {
+    this.setState({
+      [field]: +value
+    })
   }
-  eval2Handeler (e) {
-    let temp = this.state.eval2
-    temp['score'] = e.target.value
-    this.setState({eval2: temp})
-  }
-  eval3Handeler (e) {
-    let temp = this.state.eval3
-    temp['score'] = e.target.value
-    this.setState({eval3: temp})
+  handleEval = (key, value) => {
+    let newEval = this.state.eval
+    newEval[key] = +value || 0
+    this.setState({
+      eval: newEval
+    })
   }
 
-  async submitHandeler (e) {
-    let temp = await this.state.preEval
-    let temp1 = await this.state.preEval
-    let temp2 = await this.state.preEval
-    let evals = []
-    temp.score = await this.state.eval1.score
-    temp1.score = await this.state.eval2.score
-    temp2.score = await this.state.eval3.score
-    evals[0] = await temp
-    evals[1] = await temp1
-    evals[2] = await temp2
-    console.log(evals)
-    // console.log('evals ', evals)
-    // temp.score = await this.state.eval2.score
-    // evals[1] = await temp
-    // await evals.push(temp)
-    // temp.score = await this.state.eval3.score
-    // evals[2] = await temp
-    // await evals.push(temp)
-    // console.log('evals', evals)
-    // let { token } = await getCookie({ req: false })
-    // if (this.state.evals.length > 0) {
-    //   await axios.post(`/evals/criteria`, {
-    //     answer_id: this.state.answer.id,
-    //     criteria_id: e.target.id,
-    //     checker_id: this.state.checker.id,
-    //     score: e.target.value
-    //   }, {
-    //     Authorization: `Bearer ${token}`
-    //   })
-    // } else {
-    //   await axios.post(`/evals/`, {
-    //     answer_id: this.state.answer.id,
-    //     criteria_id: e.target.id,
-    //     checker_id: this.state.checker.id,
-    //     score: e.target.value,
-    //     _method: 'put'
-    //   }, {
-    //     Authorization: `Bearer ${token}`
-    //   })
-    // }
+  submitHandeler = async (e) => {
+    let finalEval = {}
+    finalEval[0] = {
+      answer_id: this.state.query.answer,
+      criteria_id: this.state.question.eval_criteria[0].id,
+      checker_id: this.state.query.id,
+      comment: this.state.comment,
+      score: this.state.eval[0]
+    }
+    finalEval[1] = {
+      answer_id: this.state.query.answer,
+      criteria_id: this.state.question.eval_criteria[1].id,
+      checker_id: this.state.query.id,
+      score: this.state.eval[1]
+    }
+    if (this.state.question.eval_criteria[2]) {
+      finalEval[2] = {
+        answer_id: this.state.query.answer,
+        criteria_id: this.state.question.eval_criteria[2].id,
+        checker_id: this.state.query.id,
+        score: this.state.eval[2]
+      }
+    }
+    if (!this.state.evals.length > 0) {
+      await axios.post('/evals/criteria', finalEval, {
+        Authorization: `Bearer ${this.state.token}`
+      })
+    } else {
+      await axios.put('/evals/criteria', finalEval, {
+        Authorization: `Bearer ${this.state.token}`
+      })
+    }
   }
 
   render () {
@@ -154,13 +148,10 @@ export default class ItimAnswer extends React.Component {
       <Grid.Row>
         <div className='container'>
           <div className='row'>
-            <div className='col-6 col-md-3'>
+            <div className='col-6 col-md-3 mb-3'>
               <ItimCard
-                fullName={
-                  this.state.itim.first_name + ' ' + this.state.itim.last_name
-                }
-                id={this.state.itim.user_id}
-                name={this.state.itim.nickname}
+                id={this.state.query.id}
+                name={this.state.query.name}
               />
             </div>
             <div className='col-12 col-md-9'>
@@ -178,8 +169,8 @@ export default class ItimAnswer extends React.Component {
               <div className='card'>
                 <div>
                   <div className='container'>
-                    <div style={{ marginTop: '2.5em' }} className='row'>
-                      <SecHeader className='mt-3'>
+                    <div className='row'>
+                      <SecHeader className='mt-3 mb-2'>
                         <Icon size='big' name={'info'} />
                         คำตอบ
                       </SecHeader>
@@ -194,50 +185,45 @@ export default class ItimAnswer extends React.Component {
                       </div>
                     </div>
                     <Box>
-                      <SecHeader className='mt-3 mb-2'>
+                      <SecHeader className='mt-1 mb-2'>
                         <Icon size='big' name={'comment outline'} />
-                          Comment
+                        ประเมิณน้อง
                       </SecHeader>
                       <div className='card'>
-                        <textarea className='form-control' />
-                      </div>
-                      <div
-                        className='row'
-                        style={{ marginTop: '20px', marginLeft: '40px', marginBottom: '30px' }}
-                      >
-                        <div
-                          className='col-md-2 col-12 mr-auto'
-                          style={{ width: '8px' }}
-                        >
-                          <div className='col-md-4'>
-                            <span>ด้านที่1</span>
-                            <input onChange={this.eval1Handeler} value={this.state.eval1['score']} className='form-control' type='number' step='0.10' />
-                          </div>
-                          <div className='col-md-4'>
-                            <span>ด้านที่2</span>
-                            <input onChange={this.eval2Handeler} value={this.state.eval2['score']} className='form-control' type='number' step='0.10' />
-                          </div>
-                          <div className='col-md-4'>
-                            <span>ด้านที่3</span>
-                            <input onChange={this.eval3Handeler} value={this.state.eval3['score']} className='form-control' type='number' step='0.10' />
-                          </div>
-                          <div
-                            style={{ paddingTop: '12px' }}
-                            className='col-md-1 col-6 mr-auto'
-                          >
-                            <div>
-                              <a onClick={this.submitHandeler} className='btn btn-success'>Submit!</a>
-                            </div>
-                          </div>
-                        </div>
+                        <textarea onChange={e => this.handleChange('comment', e.target.value)} className='form-control' placeholder={`ให้ความเห็นตรงนี้เลย!`} />
                       </div>
                     </Box>
+                    <div
+                      className='row mt-2 mb-3'
+                    >
+                      {
+                        this.state.question.eval_criteria.map((data, i) =>
+                          <div key={i} className='col-12 col-md-4 mb-2'>
+                            <span>ด้าน {data.name}</span>
+                            <input
+                              onChange={e => this.handleEval(i, e.target.value)}
+                              value={this.state.eval[i]}
+                              className='form-control'
+                              type='number'
+                              step='0.10'
+                            />
+                          </div>
+                        )
+                      }
+                    </div>
+                    <div
+                      className='col-12 mt-3 d-flex justify-content-center align-items-center'
+                    >
+                      <div>
+                        <a onClick={this.submitHandeler} className='mb-3 btn btn-warning btn-lg'>Submit!</a>
+                      </div>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>{/* END */}
+        </div>
       </Grid.Row>
     )
   }
