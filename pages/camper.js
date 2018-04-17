82294916b8f73647e7b3eaece89878022a133295
Keerati <mixxdarkz2@gmail.com>
2018-04-18 03:33:17 +0700

    [ADD] Add Camper Route

---
+++ pages/camper.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Camper from '../components/camper/Main'
+
+export default class CamperPage extends React.Component {
+  render () {
+    return (
+      <Camper />
+    )
+  }
+}
