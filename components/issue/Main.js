1949e085cc9e87cd8553d66c7ef08946b1cfc7ab
basinw <bas10600@gmail.com>
2018-04-24 01:23:29 +0700

    [UPDATE] Used react-sticky in issue/Main

--- components/issue/Main.js
+++ components/issue/Main.js
@@ -1,22 +1,30 @@
 import React from 'react'
-import { compose } from 'recompose'
+import { compose, lifecycle } from 'recompose'
 import { connect } from 'react-redux'
 import { actions as issueActions } from '../../store/modules/issue'
-import Header from '../layout/header'
 import IssueList from './IssueList'
+import Sidebar from './Sidebar'
+import Layout from '../layout/layout'
+import { StickyContainer, Sticky } from 'react-sticky'
 
 const MainIssue = (props) => (
-  <div>
-    <Header />
-    <div className='container'>
-      <div className='row'>
-        <div className='col-12'>
-          <button onClick={props.getIssue}>getIssue</button>
-          <IssueList list={props.issue.issueList} />
-        </div>
+  <Layout subheadertext='Issue Management'>
+    <div className='row'>
+      <div className='col-12 col-md-3'>
+        <StickyContainer>
+          <Sticky>
+            {
+              (p) => <Sidebar dis={p.distanceFromTop} />
+            }
+          </Sticky>
+        </StickyContainer>
+      </div>
+      <div className='col-12 col-md-9'>
+        <h2>Issue list | {`Day 1 (${props.issue.date})`}</h2>
+        <IssueList list={props.issue.issueList} />
       </div>
     </div>
-  </div>
+  </Layout>
 )
 
 export default compose(
@@ -25,5 +33,13 @@ export default compose(
       issue: state.issue
     }),
     { ...issueActions }
-  )
+  ),
+  lifecycle({
+    componentDidMount () {
+      this.props.getIssue()
+    },
+    componentWillUnmount () {
+
+    }
+  })
 )(MainIssue)
