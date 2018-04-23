6f803690a2f7a79a20ba2416d5b0c309d1d7e676
basinw <bas10600@gmail.com>
2018-04-23 20:57:49 +0700

    [UPDATE] Add class into component/issue/Main

--- components/issue/Main.js
+++ components/issue/Main.js
@@ -1,12 +1,29 @@
 import React from 'react'
 import { compose } from 'recompose'
+import { connect } from 'react-redux'
+import { actions as issueActions } from '../../store/modules/issue'
+import Header from '../layout/header'
+import IssueList from './IssueList'
 
 const MainIssue = (props) => (
   <div>
-    main issue
+    <Header />
+    <div className='container'>
+      <div className='row'>
+        <div className='col-12'>
+          <button onClick={props.getIssue}>getIssue</button>
+          <IssueList list={props.issue.issueList} />
+        </div>
+      </div>
+    </div>
   </div>
 )
 
 export default compose(
-
+  connect(
+    state => ({
+      issue: state.issue
+    }),
+    { ...issueActions }
+  )
 )(MainIssue)
