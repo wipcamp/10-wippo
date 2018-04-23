2d4c9762a352b703024cd9a8273612e96dad11c8
basinw <bas10600@gmail.com>
2018-04-24 01:43:09 +0700

    [UPDATE] Add date&time prority and action for IssueItem

--- components/issue/IssueItem.js
+++ components/issue/IssueItem.js
@@ -1,14 +1,41 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import styled from 'styled-components'
+
+const Prioritise = styled.div.attrs({
+  className: 'mx-3 p-1 rounded'
+})`
+  display: inline-block;
+  ${props => props.level === 1 ? `
+    background: red;
+    color: white;
+  ` : props.level === 2 ? `
+    background: orange;
+    color: white;    
+  ` : props.level === 3 && `
+    background: green;
+    color: white;
+  `}
+`
 
 const IssueItem = ({
   data: {
     topic
   }
 }) => (
-  <div className='row'>
-    <div className='col-12 border py-3'>
-      {topic}
+  <div className='row my-2'>
+    <div className='col-12 border pt-2 pb-1'>
+      <b>หัวข้อ:</b> {topic}
+    </div>
+    <div className='col-12 bg-secondary py-1'>
+      <div className=' text-white'>
+        วัน 30 May 2018 เวลา 18:00:01
+        <Prioritise level={3}>ปานกลาง</Prioritise>
+        <div className='d-inline-block float-right'>
+          <button className='btn btn-warning mr-1'>แก้ไข</button>
+          <button className='btn btn-info mr-1'>ดูรายละเอียด</button>
+        </div>
+      </div>
     </div>
   </div>
 )
