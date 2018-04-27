cc5e764d1ccbc8e97cc7bccdd0358e4d6418c0b8
basinw <bas10600@gmail.com>
2018-04-28 01:02:43 +0700

    [UPDATE] Imported CreateIssue into issue/Main

--- components/issue/Main.js
+++ components/issue/Main.js
@@ -5,6 +5,7 @@ import { actions as issueActions } from '../../store/modules/issue'
 import IssueList from './IssueList'
 import Sidebar from './Sidebar'
 import Layout from '../layout/layout'
+import CreateIssue from './CreateIssue'
 
 const MainIssue = (props) => (
   <Layout subheadertext='Issue Management'>
@@ -14,12 +15,13 @@ const MainIssue = (props) => (
       </div>
       <div className='col-12 col-md-9'>
         <h2>Issue list | {`Day 1 (${props.issue.date})`}</h2>
-        {
-          props.issue.loading && <div className='text-center'>Loading...</div>
-        }
-        <IssueList list={props.issue.issueList} />
+        <IssueList list={props.issue.issueList} loading={props.issue.loading}/>
       </div>
     </div>
+    <CreateIssue
+      show={props.issue.showModal}
+      toggle={props.toggleModal}
+    />
   </Layout>
 )
 
