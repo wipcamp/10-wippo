694e970e1374be7f99c0f657a14872a059b85523
basinw <bas10600@gmail.com>
2018-04-27 18:32:57 +0700

    [UPDATE] Reverse issue data from store

--- store/modules/issue.js
+++ store/modules/issue.js
@@ -29,7 +29,7 @@ export default (state = initialState, action) => {
       return {
         ...state,
         loading: false,
-        issueList: action.data
+        issueList: action.data.reverse()
       }
 
     case GET_PROBLEM.REJECTED:
