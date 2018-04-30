a9b26f8ee479f32c947151054e652dc4b5c83890
basinw <bas10600@gmail.com>
2018-04-30 14:39:44 +0700

    [UPDATE] Component issue/IssueList
    Add filter function

--- components/issue/IssueList.js
+++ components/issue/IssueList.js
@@ -1,12 +1,79 @@
 import React from 'react'
 import IssueItem from './IssueItem'
 import PropTypes from 'prop-types'
+import moment from 'moment'
 
-const IssueList = ({ list, loading }) => (
+const issueFilter = ({
+  list,
+  priority,
+  time,
+  date,
+  solve
+}) => {
+  return list.filter(item => {
+    let shouldReturn = true
+    // filter by priority
+    switch (priority) {
+      case '1':
+        if (item.priority_id !== 1) shouldReturn = false
+        break
+      case '2':
+        if (item.priority_id !== 2) shouldReturn = false
+        break
+      case '3':
+        if (item.priority_id !== 3) shouldReturn = false
+        break
+    }
+
+    // filter by time
+    if (time !== 'all') {
+      let t1 = time.substring(0, 2)
+      let h = moment(item.created_at).format('hh')
+      if (t1 !== h) {
+        shouldReturn = false
+      }
+    }
+
+    // filter by date
+    if (date !== 'all') {
+      let dateMoment = moment(item.created_at).format('DD MMM YYYY')
+      if (dateMoment !== date) {
+        shouldReturn = false
+      }
+    }
+
+    // filter by isSolve
+    switch (solve) {
+      case '1':
+        if (!(item.is_solve || item.not_solve)) shouldReturn = false
+        break
+      case '0':
+        if (item.is_solve || item.not_solve) shouldReturn = false
+        break
+    }
+
+    return shouldReturn
+  })
+}
+
+const IssueList = ({
+  list,
+  loading,
+  priority,
+  time,
+  date,
+  solve
+}) => (
   <div>
     {loading && <div className='text-center'>Loading...</div>}
     {
-      list.map((d, i) => (
+      issueFilter({
+        list,
+        priority,
+        time,
+        date,
+        solve
+      }).map((d, i) => (
         <IssueItem data={d} key={i} />
       ))
     }
@@ -15,7 +82,11 @@ const IssueList = ({ list, loading }) => (
 
 IssueList.propTypes = {
   list: PropTypes.array.isRequired,
-  loading: PropTypes.bool.isRequired
+  loading: PropTypes.bool.isRequired,
+  priority: PropTypes.string.isRequired,
+  time: PropTypes.string.isRequired,
+  date: PropTypes.string.isRequired,
+  solve: PropTypes.string.isRequired
 }
 
 export default IssueList
