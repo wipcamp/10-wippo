5350c71bce7d5975206c4c411d239654b83de098
basinw <bas10600@gmail.com>
2018-05-01 20:02:22 +0700

    [UPDATE] module issue.edit
    Update module issue.edit add SET_SOLVE_STATUS action

--- store/modules/issue.edit.js
+++ store/modules/issue.edit.js
@@ -8,13 +8,15 @@ const SET_FIELD = issueAction('SET_FIELD')
 const EDIT_ISSUE = issueAction('EDIT_ISSUE', true)
 const TOGGLE_MODAL = issueAction('TOGGLE_MODAL')
 const INIT_EDIT = issueAction('INIT_EDIT')
+const SET_SOLVE_STATUS = issueAction('SET_SOLVE_STATUS')
 
 let initialState = {
   topic: '',
   desc: '',
-  type: '',
-  priority: '',
-  isSolve: '',
+  type: 0,
+  priority: 0,
+  isSolve: 0,
+  notSolve: 0,
   assignTo: [],
   showModal: false
 }
@@ -39,6 +41,13 @@ export default (state = initialState, action) => {
         ...action.data,
         showModal: true
       }
+
+    case SET_SOLVE_STATUS:
+      return {
+        ...state,
+        [action.field1]: action.value1,
+        [action.field2]: action.value2
+      }
     default: return state
   }
 }
@@ -60,5 +69,32 @@ export const actions = {
   }),
   toggleModal: () => ({
     type: TOGGLE_MODAL
-  })
+  }),
+  changeSolveStatus: (value) => (dispatch) => {
+    if (value === '1') { // isSolve = true
+      dispatch({
+        type: SET_SOLVE_STATUS,
+        field1: 'isSolve',
+        value1: 1,
+        field2: 'notSolve',
+        value2: 0
+      })
+    } else if (value === '2') { // notSolve = true
+      dispatch({
+        type: SET_SOLVE_STATUS,
+        field1: 'isSolve',
+        value1: 0,
+        field2: 'notSolve',
+        value2: 1
+      })
+    } else { // isSolve = false, notSolve = false
+      dispatch({
+        type: SET_SOLVE_STATUS,
+        field1: 'isSolve',
+        value1: 0,
+        field2: 'notSolve',
+        value2: 0
+      })
+    }
+  }
 }
