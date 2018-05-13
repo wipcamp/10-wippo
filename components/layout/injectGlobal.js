0fb1b2b6ffbcf37dc252d9c4783c97df2d76edb2
Keerati <mixxdarkz2@gmail.com>
2018-05-13 23:02:07 +0700

    [UPDATE] Importatn font

--- components/layout/injectGlobal.js
+++ components/layout/injectGlobal.js
@@ -2,7 +2,7 @@ import {injectGlobal} from 'styled-components'
 
 export default injectGlobal`
   *, body, input, h1, h2, h3, h4, h5 .btn {
-    font-family: 'Prompt', sans-serif;
+    font-family: 'Prompt', sans-serif !important;
   }
   body, input, textarea {
     font-size: 12px;
