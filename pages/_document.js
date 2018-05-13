8f6c48854831ecb346a7c16fb748081e679cd1b4
Keerati <mixxdarkz2@gmail.com>
2018-05-13 22:17:40 +0700

    [UPDATE] Update Background and Favicon

--- pages/_document.js
+++ pages/_document.js
@@ -23,6 +23,11 @@ export default class MyDocument extends Document {
           <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
           <meta httpEquiv="X-UA-Compatible" content="ie=edge"/>
           <title>Wippo | WIP Camp #10</title>
+          <link rel="shortcut icon" href="/static/img/favicon/favicon.ico" type="image/x-icon"/>
+          <link rel="apple-touch-icon" sizes="60x60" href="/apple-touch-icon.png"/>
+          <link rel="icon" href="/static/img/favicon.ico" type="/image/x-icon"/>
+          <link rel="icon" type="image/png" sizes="32x32" href="/static/img/favicon/favicon-32x32.png"/>
+          <link rel="icon" type="image/png" sizes="16x16" href="/static/img/favicon/favicon-16x16.png"/>
           <link href="https://fonts.googleapis.com/css?family=Prompt" rel="stylesheet" />
           <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossOrigin="anonymous" />
           <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.12/semantic.min.css" />
