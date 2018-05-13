a5c7c747480a65a7b236758d61b80da32d3bd76f
basinw <bas10600@gmail.com>
2018-05-13 18:50:28 +0700

    merge

---
+++ components/register/FormRegis.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { compose, withProps } from 'recompose'
+import FormContainer from './FormContainer'
+import { fields as field1 } from './form.json'
+import { fields as field2 } from './form2.json'
+
+const fields = [
+  ...field1,
+  {
+    component: 'header',
+    label: 'ประสบการณ์'
+  },
+  ...field2
+]
+
+const StepOne = (props) => {
+  return (
+    <FormContainer
+      {...props}
+      fields={fields}
+      buttonText={`ลงทะเบียน`}
+    />
+  )
+}
+
+export default compose(
+  withProps(
+    props => ({
+      onSubmit: (d) => {console.log(d)}
+      // onSubmit: props.saveRegisterStep1
+    })
+  )
+)(StepOne)
