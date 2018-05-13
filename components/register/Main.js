3ddad22d3d4c00a0b58698b322aea89a79a64952
basinw <bas10600@gmail.com>
2018-05-13 18:45:45 +0700

    [ADD] Add component for register page

---
+++ components/register/Main.js
@@ -0,0 +1,79 @@
+import React from 'react'
+import { compose, lifecycle, withProps } from 'recompose'
+import { reduxForm } from 'redux-form'
+import { connect } from 'react-redux'
+import styled from 'styled-components'
+import Router from 'next/router'
+
+import {actions as registerActions} from '../../store/modules/register'
+import FormRegis from './FormRegis'
+import Alert from '../core/Alert'
+import { validate } from '../core/validateForm'
+
+const Image = styled.img`
+  max-width: 340px;
+`
+
+const BackgroundContainer = styled.div`
+  background-color: #5eb9e2;
+  min-height: 100vh;
+  height: auto;
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-attachment: fixed;
+`
+
+export const MainRegister = props => {
+  const { regis } = props
+  return (
+    <div>
+      <BackgroundContainer>
+        <div className='container'>
+          <Alert
+            showDialog={regis.showDialog}
+            error={regis.error}
+            message={regis.dialogMessage}
+            hideDialog={props.hideDialog}
+          />
+          <div className='row '>
+            <div className='col-12 mt-4 col-md-6 mx-auto text-center justify-content-center'>
+              <Image src='/static/img/logofinals.png' className='img-fluid mb-3' alt='wipcamp-logo' />
+            </div>
+            <div className='col-12 col-md-10 mx-auto text-center' >
+              <FormRegis {...props} />
+            </div>
+          </div>
+        </div>
+      </BackgroundContainer>
+    </div>
+  )
+}
+
+export default compose(
+  connect(
+    state => ({
+      regis: state.register
+    }),
+    { ...registerActions }
+  ),
+  withProps(
+    props => ({
+      initialValues: {
+        known_via: '-',
+        past_camp: '-'
+      }
+    })
+  ),
+  reduxForm({
+    form: 'register',
+    validate,
+    onSubmitFail: (err, __, ___, props) => props.onSubmitError(err)
+  }),
+  lifecycle({
+    componentWillReceiveProps (nextProps) {
+      if (nextProps.registerData.registerStep > 1) {
+        Router.push('/dashboard')
+      }
+    }
+  })
+)(MainRegister)
