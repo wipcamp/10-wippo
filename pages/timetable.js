011f2bf474dcc7362223b4b34b2d33df150a1030
Keerati <mixxdarkz2@gmail.com>
2018-05-14 20:50:52 +0700

    [ADD] Add Sample Calendar

---
+++ pages/timetable.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import TimetablePage from '../components/timetable/Main'
+
+export default class Timetable extends React.Component {
+  render () {
+    return (
+      <TimetablePage />
+    )
+  }
+}
