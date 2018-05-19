a7408f106d470fc41590697309ed4c84508d4ae9
ewqdsacxz <Patric_emmel@outlook.com>
2018-05-19 15:06:33 +0700

    [UPDATE] modal css

--- components/timetable/Modal.js
+++ components/timetable/Modal.js
@@ -1,6 +1,14 @@
 import React from 'react'
 import Modal from '../issue/Modal'
 import DatePicker from 'react-datepicker'
+import styled from 'styled-components'
+
+const ButtonContainer = styled.div`
+  padding:8px;
+  padding-top:50px;  
+  margin: auto;
+  width: 50%;
+`
 
 export default (isOpen, eventName, description, location, startOn, finishOn, createBy, handleDate) => {
   return (
@@ -20,7 +28,14 @@ export default (isOpen, eventName, description, location, startOn, finishOn, cre
           <div className='row'>
             <div className='col-6'>
             location
+              <input type={'text'} className={'form-control'} />
             </div>
+            <div className='col-6'>
+              createBy
+              <input type={'text'} className={'form-control'} />
+            </div>
+          </div>
+          <div className='row'>
             <div className='col-6'>
             StartOn
               <DatePicker
@@ -33,14 +48,26 @@ export default (isOpen, eventName, description, location, startOn, finishOn, cre
                 timeCaption='time'
               />
             </div>
-          </div>
-          <div className='row'>
             <div className='col-6'>
             FinishOn
+              <DatePicker
+                selected={startOn}
+                onChange={handleDate}
+                showTimeSelect
+                timeFormat='HH:mm'
+                timeIntervals={15}
+                dateFormat='LLL'
+                timeCaption='time'
+              />
             </div>
-            <div className='col-6'>
-            CreateBy
-            </div>
+          </div>
+          <div className='row'>
+            <ButtonContainer style={{textAlign: 'right'}}>
+              <button className='btn btn-success'>save</button>
+            </ButtonContainer>
+            <ButtonContainer>
+              <button className='btn btn-danger'>cancel</button>
+            </ButtonContainer>
           </div>
         </form>
       </div>
