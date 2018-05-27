969b689ad5bae316c703dc8af6a8d35ae8c29b21
Keerati <mixxdarkz2@gmail.com>
2018-05-28 03:48:02 +0700

    [UPDATE] Update UI and Link to Playstore

--- components/complete/Main.js
+++ components/complete/Main.js
@@ -13,8 +13,17 @@ export default class Main extends React.Component {
       <IndexTemplate>
         <Wrapper>
           <h1>Register Complete!</h1>
-          <h3>Thanks for Register</h3>
-          <h6>redirect to home...</h6>
+          <h3>Download Application on PlayStore!</h3>
+          <br />
+          <div className='row'>
+            <div className='col-12'>
+              <a href='https://play.google.com/store/apps/details?id=sit.kmutt.wipmobile' className='btn btn-lg btn-success mr-3'>
+                <i className='fab fa-android mr-3' />
+              Download Android on PlayStore
+              </a>
+              <a href='/' className='btn btn-lg btn-primary'>Home</a>
+            </div>
+          </div>
         </Wrapper>
       </IndexTemplate>
     )
